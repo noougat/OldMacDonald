df3524b5151a01ca77c21173080ce57e0fe842ca
noougat <jelam8@s.sfusd.edu>
2017-10-20 14:10:35 -0700

    fixed the problem

--- NamedCow.java
+++ NamedCow.java
@@ -1,10 +1,13 @@
 class NamedCow extends Cow
 {     
   private String myName;
-  public NamedCow(String name, String type, String sound) {         
+  public NamedCow(String name, String type, String sound) {      
+    myType = type;         
+    mySound = sound;   
     myName = name;
   }     
-  public NamedCow() {         
+  public NamedCow() {  
+
     myName = "unknown";
   }      
   public String getName() 
