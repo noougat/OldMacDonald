f46ef1a3ff748e3cb06027421be43427994446be
noougat <jelam8@s.sfusd.edu>
2017-10-20 13:54:11 -0700

    removed extra

--- NamedCow.java
+++ NamedCow.java
@@ -2,13 +2,9 @@ class NamedCow extends Cow
 {     
   private String myName;
   public NamedCow(String name, String type, String sound) {         
-    myType = type;         
-    mySound = sound;
     myName = name;
   }     
   public NamedCow() {         
-    myType = "unknown";         
-    mySound = "unknown";
     myName = "unknown";
   }      
   public String getName() 
